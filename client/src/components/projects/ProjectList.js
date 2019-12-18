0b2c6218c35193eb9922f4792182df7966d0ccef
maxnvdm <maxnvdm@gmail.com>
2019-12-18 09:44:06 +0200

    Got the database connected to the project list (display properties)

--- client/src/components/projects/ProjectList.js
+++ client/src/components/projects/ProjectList.js
@@ -1,16 +1,61 @@
 /**Lists project (property listing) details */
-import React from 'react'
-import ProjectSummary from './ProjectSummary'
+import React, { Component } from 'react'
+// import ProjectSummary from './ProjectSummary'
 import {Link} from 'react-router-dom'
+import axios from 'axios';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
 
-const ProjectList= ()=>{
-    return(
-        <div className="project-list section">
-            <ProjectSummary/>
-            <ProjectSummary/>
+const ProjectSummary = props =>(
+    <div>
+      <Card>
+        <CardImg top width="100%" src="/img/penthouse.jpg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{props.property.name}</CardTitle>
+          <CardSubtitle>{props.property.price}</CardSubtitle>
+          <CardText>{props.property.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+)
 
-        </div>
-    )
+class ProjectList extends Component{
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          properties: []
+        }
+      }
+    
+    componentDidMount() {
+        axios.get('http://localhost:4000/api')
+            .then(response => {
+                console.log(response.data);
+                console.log(response.headers);
+                this.setState({ properties: response.data })
+            })
+            .catch((error) => {
+            console.log(error);
+            })
+        
+    }
+
+    propertyList() {
+        return this.state.properties.map(property => {
+            return <ProjectSummary property={property} key={property._id}/>;
+          })
+    }
+    
+    render() {
+        return(
+            <div className="project-list section">
+                { this.propertyList() }
+            </div>
+        )
+    }
 }
 
 export default ProjectList
--- client/src/components/projects/ProjectSummary.js
+++ client/src/components/projects/ProjectSummary.js
@@ -1,27 +1,51 @@
 /*Provides template for project details to show */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button
   } from 'reactstrap';
+import axios from 'axios';
 
+class ProjectSummary extends Component{
+  constructor(props) {
+    super(props);
 
-const ProjectSummary = ()=>{
+    this.state = {
+      properties: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/api')
+      .then(response => {
+        console.log(response.data);
+        console.log(response.headers);
+        this.setState({ properties: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      
+  }
+
+  render(){
     return(
-
-        <div>
-      <Card>
-        <CardImg top width="100%" src="/img/penthouse.jpg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Rondebosch, R1 650 000</CardTitle>
-          <CardSubtitle>3 bedroom penthouse</CardSubtitle>
-          <CardText>Located in the good place close to the good things etcetera...</CardText>
-        </CardBody>
-      </Card>
-    </div>
-        
-    )
+      <div>
+        <Card>
+          <CardImg top width="100%" src="/img/penthouse.jpg" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{ this.state.properties.map((property) => {
+              return <p>{property.name}</p>}) }</CardTitle>
+            <CardSubtitle>3 bedroom penthouse</CardSubtitle>
+            <CardText>Located in the good place close to the good things etcetera...</CardText>
+          </CardBody>
+        </Card>
+      </div>
+          
+      )
+  }
+   
 }
 
 export default ProjectSummary

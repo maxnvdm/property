6bc574087b124a69ecf17d75d480bdcccb5058f9
maxnvdm <maxnvdm@gmail.com>
2019-12-20 20:29:01 +0200

    Configuring build settings for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const express = require('express');
- const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+const path = require('path');
 
 // TODO: implement the following
 const authRoutes = require('./routes/auth');
@@ -34,6 +35,9 @@ app.use(passport.session());
 app.use(bodyParser.json());
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/api', require('./routes/api'));
 app.use('/auth', require('./routes/auth'));
 app.use("/testAPI", require('./routes/testAPI'));
@@ -45,7 +49,8 @@ app.use(function(err, req, res, next){
 });
 
 // listen for requests
-app.listen(process.env.port || 4000, function(){
-    console.log('now listening for requests');
+const port = process.env.PORT || 4000;
+app.listen(port, function(){
+  console.log(`Password generator listening on ${port}`);
 });
 
